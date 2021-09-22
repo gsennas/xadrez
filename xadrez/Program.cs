5c6ead25008894f0e0d8f522f2c214a60266f9bc
Glaucio Senna <gsennaeua@gmail.com>
2021-09-22 14:35:31 -0300

    Melhora do Metodo Imprimir Tabuleiro

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -8,17 +8,17 @@ namespace xadrez
     {
         static void Main(string[] args)
         {
-
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine( pos);
-            Console.WriteLine(pos.toPosicao());
-            
-            /*try
+            /*t
+           PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+           Console.WriteLine( pos);
+           Console.WriteLine(pos.toPosicao());
+            */
+            try
             {
                 Tabuleiro tabuleiro = new Tabuleiro(8, 8);// instanciar matriz = null em todos os elementos
-                tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 9));
+                tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
                 tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(0, 4));
-                tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(1, 3));
+                tabuleiro.ColocarPeca(new Torre(Cor.branca, tabuleiro), new Posicao(1, 3));
                 tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(2, 4));
 
                 Tela.ImprimirTabuleiro(tabuleiro);
@@ -27,8 +27,8 @@ namespace xadrez
             catch (TabuleiroExeption e)
             {
                 Console.WriteLine(e.Message);
-            }*/
             }
+        }
 
 
     }
