1ee054d04312da6c00b67f3b3f9e16b4422e7314
Glaucio Senna <gsennaeua@gmail.com>
2021-09-22 12:26:31 -0300

    Classe Posicao - criacao

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using xadrez.tabuleiro;
 
 namespace xadrez
 {
@@ -6,7 +7,8 @@ namespace xadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P = new Posicao(3, 4);
+            Console.WriteLine("Posicao:" + P);
         }
     }
 }
